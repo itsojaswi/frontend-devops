73d7ac1233b3bde0cdd4abc42b7249feac31809c
Your Name <you@example.com>
2024-07-12 10:17:31 +0545

    backend

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:4000",
+        target: "http://3.0.183.83:4000",
         changeOrigin: true,
       },
     },
